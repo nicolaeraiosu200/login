ce1a506ed02d159a680f8f94d1e756c38f5b4867
nicolaeraiosu200 <nicolae.raiosu2022@gmail.com>
2025-05-29 17:58:09 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,21 +1,15 @@
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
+require_once '../includes/auth.php';
 
-if (!file_exists('../data/users.txt')) {
-    file_put_contents('../data/users.txt', '');
+$username = sanitizeInput($_POST['username']);
+$password = sanitizeInput($_POST['password']);
+
+if (strlen($password) < 6) {
+    header("Location: ../index.html?error=" . urlencode('Parola trebuie să aibă minim 6 caractere'));
+    exit();
 }
 
-$users = file('../data/users.txt', FILE_IGNORE_NEW_LINES);
-foreach ($users as $user) {
-    list($savedUser, $savedPass) = explode(':', $user);
-    if ($savedUser === $username) {
-        die('User already exists!');
-    }
-}
-
-file_put_contents('../data/users.txt', "$username:$password" . PHP_EOL, FILE_APPEND);
-
-header('Location: ../dashboard.html');
+saveUser($username, $password);
+header("Location: ../dashboard.html?username=" . urlencode($username));
 exit();
 ?>
