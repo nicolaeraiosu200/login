020e7857fbc9da07ba0d234d8460dcd64063cf8c
nicolaeraiosu200 <nicolae.raiosu2022@gmail.com>
2025-05-29 18:01:00 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
-require_once '../includes/auth.php';
+require_once 'https://nicolaeraiosu200.github.io/auth.php';
 
 $username = sanitizeInput($_POST['username']);
 $password = sanitizeInput($_POST['password']);
 
 if (verifyUser($username, $password)) {
-    header("Location: ../dashboard.html?username=" . urlencode($username));
+    header("https://nicolaeraiosu200.github.io/dashboard.html?username=" . urlencode($username));
     exit();
 } else {
-    header("Location: ../index.html?error=" . urlencode('Utilizator sau parolă incorectă'));
+    header("https://nicolaeraiosu200.github.io/index.html?error=" . urlencode('Utilizator sau parolă incorectă'));
     exit();
 }
 ?>
